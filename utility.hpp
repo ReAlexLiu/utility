ca18232dac7693ca1ba9c29a470adad23aa89f33
刘清泉 <bigtooth2006@gmail.com>
2024-04-07 10:57:08 +0800

    添加字符串分割

--- utility.hpp
+++ utility.hpp
@@ -262,6 +262,29 @@ public:
         }
         return dst_str;
     }
+
+    // 字符串分割函数
+    static std::vector<std::string> split(std::string str, std::string pattern)
+    {
+        std::string::size_type   pos;
+        std::vector<std::string> result;
+        str += pattern;  // 扩展字符串以方便操作
+        int size = str.size();
+        for (int i = 0; i < size; i++)
+        {
+            pos = str.find(pattern, i);
+            if (pos < size)
+            {
+                std::string s = str.substr(i, pos - i);
+                if (!s.empty())
+                {
+                    result.push_back(s);
+                    i = pos + pattern.size() - 1;
+                }
+            }
+        }
+        return result;
+    }
 };
 }
 #ifdef __GNUC__
